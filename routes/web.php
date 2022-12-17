0e3680963eb940ab54f2136fec319cb1c9dd93dc
Cư Hoàng Ngọc <20520145@gm.uit.edu.vn>
2022-12-17 18:53:20 +0700

    update routing

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
-use App\Http\Controllers\AdminBusCompaniesController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminHomeController;
+use App\Http\Controllers\HeadAdminController;
 use App\Http\Controllers\AdminBusesController;
-use App\Http\Controllers\AdminNormalUsersController;
-use App\Http\Controllers\AdminRoutesController;
-use App\Http\Controllers\AdminTicketsController;
-use App\Http\Controllers\AdminStatisticsController;
 use App\Http\Controllers\AdminTripsController;
+use App\Http\Controllers\AdminNormalUsersController;
+use App\Http\Controllers\AdminBusCompaniesController;
+use App\Http\Controllers\AdminTicketDetailsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,10 +23,13 @@ use App\Http\Controllers\AdminTripsController;
 */
 
 Route::get('/', AdminHomeController::class)->middleware(['auth'])->name('home.index');
-Route::get('/admin', AdminHomeController::class)->middleware(['auth'])->name('admin.index');
+Route::get('/admin/show', HeadAdminController::class)->middleware(['auth'])->name('admin.show');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard', [
+        'user' => Auth::user(),
+        'Ten_NX' => Auth::user()->IdNX == NULL ? 'Quản trị viên hệ thống' : Auth::user()->busCompany->Ten_NX
+    ]);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -37,22 +40,10 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__ . '/auth.php';
 
-// Route::group(['nomal_user', 'middleware' => ['auth', 'verified']], function () {
-//     Route::get('/normal_users', [AdminNormalUsersController::class, 'index'])->name('normal_users.index');
-//     Route::get('/normal_users/create', [AdminNormalUsersController::class, 'create'])->name('normal_users.create');
-//     Route::get('/normal_users/{IdUser}', [AdminNormalUsersController::class, 'show'])->name('normal_users.show');
-//     Route::delete('/normal_users/{IdUser}', [AdminNormalUsersController::class, 'destroy'])->name('normal_users.destroy');
-// });
-
-
-
 Route::resources([
     'normal_users' => AdminNormalUsersController::class,
     'bus_companies' => AdminBusCompaniesController::class,
     'buses' => AdminBusesController::class,
     'trips' => AdminTripsController::class,
-    // 'routes' => AdminRoutesController::class,
-    // 'tickets' => AdminTicketsController::class,
+    'ticket_details' => AdminTicketDetailsController::class,
 ]);
-
-Route::get('/normal_users/search', [AdminNormalUsersController::class, 'search'])->name('normal_users.search');
